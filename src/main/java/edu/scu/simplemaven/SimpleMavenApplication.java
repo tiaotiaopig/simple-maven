bc1bbd813d4d3a46f07708340170d8acd29261cc
LiFeng <2807229316@qq.com>
2022-08-24 20:30:48 +0800

    initial commit!

--- src/main/java/edu/scu/simplemaven/SimpleMavenApplication.java
+++ src/main/java/edu/scu/simplemaven/SimpleMavenApplication.java
@@ -7,4 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleMavenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleMavenApplication.class, args
+        SpringApplication.run(SimpleMavenApplication.class, args);
+    }
+
+}
--- src/test/java/edu/scu/simplemaven/SimpleMavenApplicationTests.java
+++ src/test/java/edu/scu/simplemaven/SimpleMavenApplicationTests.java
@@ -8,3 +8,6 @@ class SimpleMavenApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+}
