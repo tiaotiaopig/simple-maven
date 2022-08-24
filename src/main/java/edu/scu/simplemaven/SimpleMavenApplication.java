dd651a130299848fa0126e3f03c91569482a6d0c
LiFeng <2807229316@qq.com>
2022-08-24 20:27:39 +0800

    Initial commit

---
+++ src/main/java/edu/scu/simplemaven/SimpleMavenApplication.java
@@ -0,0 +1,10 @@
+package edu.scu.simplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMavenApplication.class, args
---
+++ src/test/java/edu/scu/simplemaven/SimpleMavenApplicationTests.java
@@ -0,0 +1,10 @@
+package edu.scu.simplemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
